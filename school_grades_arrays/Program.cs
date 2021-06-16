777363f7d673a04bf8ef382fd1dab9b871626048
Dustin Fox <djf149@hotmail.com>
2021-06-16 16:41:17 -0400

    Add project files.

---
+++ school_grades_arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace school_grades_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            #region format 1
+            //var studentGrade = new int[10];
+
+            //studentGrade[0] = 10;
+            //studentGrade[1] = 20;
+            //studentGrade[2] = 30;
+            //studentGrade[3] = 40;
+            //studentGrade[4] = 50;
+            //studentGrade[5] = 60;
+            //studentGrade[6] = 80;
+            //studentGrade[7] = 90;
+            //studentGrade[8] = 100;
+            //studentGrade[9] = 110;
+            //Console.WriteLine(studentGrade[4]);
+            #endregion
+
+            var studentGrades = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 };
+
+            foreach (var studentGrade in studentGrades)
+            {
+            Console.WriteLine(studentGrade);
+
+            }
+
+        }
+    }
+}
